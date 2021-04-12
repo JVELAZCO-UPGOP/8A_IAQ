4f39fe70ff61c90be267c0a6ca19e27c28d5e17c
Neo04 <neo_aq@yahoo.com>
2021-04-12 13:57:47 -0500

    Delete y Update en la API

--- NODEJS/enrutador.js
+++ NODEJS/enrutador.js
@@ -23,6 +23,34 @@ module.exports={
             global.recursos.mascotas.push(data.payload);
             callback(201,data.payload);
         },
+        put: (data, callback)=>{
+            if (data.indice){
+
+            if(global.recursos.mascotas[data.indice])
+            {
+                global.recursos.mascotas[data.indice]= data.payload;
+               return callback(200,global.recursos.mascotas[data.indice]);
+
+            }
+            return callback(404, {mensaje:"indice no encontrado"});
+            }
+            callback(400, {mensaje:"Indice no encontrado"});
+        },
+        delete: (data, callback)=>{
+            if (data.indice){
+
+            if(global.recursos.mascotas[data.indice])
+            {
+                global.recursos.mascotas=global.recursos.mascotas.filter((_mascota, indice)=>
+                 indice!=data.indice);
+               return callback(204,{mensaje:"Elemento eliminado"});
+
+
+            }
+            return callback(404, {mensaje:"indice no encontrado"});
+            }
+            callback(400, {mensaje:"Indice no encontrado"});
+        }
     },
     noEncontrado:(data, callback)=>{
      
